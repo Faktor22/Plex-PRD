8fdb379799f02a14cea6dabb5a99e2d8c34b7753
Amir Fahimnia <info@faktor22.nl>
2021-12-30 11:21:59 +0100

    • Some styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-// topbar active button
+/*==========  Progress indicator  =========*/
+
+window.onscroll = function() { scrollIndicator() };
+
+function scrollIndicator() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 80;
+    document.getElementById("progress-indicator").style.width = scrolled + "%";
+
+    console.log(scrolled);
+}
+
+
+/*==========  Topbar active button  =========*/
+
 let topBar = document.querySelectorAll(".topbar nav li a");
 
 topBar.forEach(a => {
@@ -8,7 +23,7 @@ topBar.forEach(a => {
     })
 })
 
-
+/*==========  Topbar active button  =========*/
 
 // let topbar = document.querySelectorAll(".topbar nav li a");
 
