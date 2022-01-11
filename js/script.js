da288cf2820795093f0eb71aba0ee99f1ec12268
Amir Fahimnia <info@faktor22.nl>
2022-01-11 21:52:17 +0100

    • Add leftMenu in script • Add Priority to User Stories

--- js/script.js
+++ js/script.js
@@ -26,19 +26,8 @@ topBar.forEach(a => {
 //     })
 // }
 
-/*==========  LeftMenu active button  =========*/
 
-let leftMenu = document.querySelectorAll(".navigation nav ul li");
-
-leftMenu.forEach(li => {
-    li.addEventListener("click", () => {
-        leftMenu.forEach(li => li.classList.remove("active"))
-        li.classList.add("active")
-    })
-})
-
-
-
+/*==========  LeftMenu extends  =========*/
 
 class sideMenu extends HTMLElement {
     connectedCallback() {
@@ -47,28 +36,28 @@ class sideMenu extends HTMLElement {
         <div class="navigation">
             <nav>
                 <ul>
-                    <li>
+                    <li id="link">
                         <a href="contributors.html">
                             <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
                         </a>
                     </li>
-                    <li class="active">
+                    <li id="link1" class="active">
                         <a href="index.html#intro">
                             <span class="icon"><ion-icon name="document-text-outline"></ion-icon></span>
                         </a>
                     </li>
-                    <li>
+                    <li id="link2">
                         <a href="design.html">
                             <span class="icon"><ion-icon name="color-palette-outline"></ion-icon></span>
                             <span class="title"></span>
                         </a>
                     </li>
-                    <li>
+                    <li id="link3">
                         <a href="code.html">
                             <span class="icon"><ion-icon name="code-slash-outline"></ion-icon></span>
                         </a>
                     </li>
-                    <li>
+                    <li id="link4">
                         <a href="scope.html">
                             <span class="icon"><ion-icon name="grid-outline"></ion-icon></span>
                         </a>
@@ -82,3 +71,32 @@ class sideMenu extends HTMLElement {
 }
 
 customElements.define('sidebar-menu', sideMenu)
+
+
+/*==========  LeftMenu active button  =========*/
+var menuLinks = window.location.pathname.split('/')[1];
+// console.log(menuLinks);
+
+switch (menuLinks) {
+    case 'index.html':
+        document.getElementById("link1").classList.add("active");
+        document.getElementById("link").classList.remove("active");
+        break;
+    case 'contributors.html':
+        document.getElementById("link").classList.add("active");
+        document.getElementById("link1").classList.remove("active");
+        break;
+    case 'design.html':
+        document.getElementById("link2").classList.add("active");
+        document.getElementById("link1").classList.remove("active");
+        break;
+    case 'code.html':
+        document.getElementById("link3").classList.add("active");
+        document.getElementById("link1").classList.remove("active");
+        break;
+    case 'scope.html':
+        document.getElementById("link4").classList.add("active");
+        document.getElementById("link1").classList.remove("active");
+        break;
+    default:
+}
