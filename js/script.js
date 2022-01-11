2eab3c437ef73a17524a9cfb9a1df0e1726955ae
Amir Fahimnia <info@faktor22.nl>
2022-01-11 18:20:07 +0100

    • Add content • Add images

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/*==========  Progress indicator  =========*/
-
-window.onscroll = function() { scrollIndicator() };
-
-function scrollIndicator() {
-    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrolled = (winScroll / height) * 80;
-    document.getElementById("progress-indicator").style.width = scrolled + "%";
-
-    console.log(scrolled);
-}
-
-
 /*==========  Topbar active button  =========*/
 
 let topBar = document.querySelectorAll(".topbar nav li a");
@@ -39,3 +25,60 @@ topBar.forEach(a => {
 //         a.classList.remove("active-topbar")
 //     })
 // }
+
+/*==========  LeftMenu active button  =========*/
+
+let leftMenu = document.querySelectorAll(".navigation nav ul li");
+
+leftMenu.forEach(li => {
+    li.addEventListener("click", () => {
+        leftMenu.forEach(li => li.classList.remove("active"))
+        li.classList.add("active")
+    })
+})
+
+
+
+
+class sideMenu extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <a href="index.html"><img src="../img/logo.png" alt="Logo Plex" class="logo" /></a>
+        <div class="navigation">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="contributors.html">
+                            <span class="icon"><ion-icon name="people-outline"></ion-icon></span>
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="index.html#intro">
+                            <span class="icon"><ion-icon name="document-text-outline"></ion-icon></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="design.html">
+                            <span class="icon"><ion-icon name="color-palette-outline"></ion-icon></span>
+                            <span class="title"></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="code.html">
+                            <span class="icon"><ion-icon name="code-slash-outline"></ion-icon></span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="scope.html">
+                            <span class="icon"><ion-icon name="grid-outline"></ion-icon></span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="version">v.1.0</p>
+            </nav>
+        </div>
+        `
+    }
+}
+
+customElements.define('sidebar-menu', sideMenu)
